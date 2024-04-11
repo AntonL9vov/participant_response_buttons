badb030935a491d56f6b376a996edd032a8792f2
Anton Lvov <lvov.anton.02@gmail.com>
2024-04-11 16:28:14 +0300

    Added base layouts, changed configs

---
+++ frontend/src/shared/config/index.ts
@@ -0,0 +1,8 @@
+export type Theme = "light" | "dark";
+
+export const THEME: Record<string, Theme> = {
+  light: "light",
+  dark: "dark",
+};
+
+export const ASSETS_PATH = "/src/shared/ui/assets/";
