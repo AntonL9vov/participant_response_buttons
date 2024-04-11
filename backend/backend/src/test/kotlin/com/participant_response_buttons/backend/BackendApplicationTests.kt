e9067754acaae79d56e06bbb5d4760fef0d999be
AntonL9vov <lvov.anton.02@gmail.com>
2024-04-11 11:06:14 +0300

    Created structure

---
+++ backend/backend/src/test/kotlin/com/participant_response_buttons/backend/BackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.participant_response_buttons.backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
